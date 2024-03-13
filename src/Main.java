22658a3852f30457eff801a2e6a15fd3e9cd44b3
jenah <jenahah85@mail.ru>
2024-03-13 20:42:30 +0300

    Вывод семи сегментов

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello world!");
+        JFrame f = new JFrame("Swing Paint Demo");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        MyPanel myPanel = new MyPanel();
+        myPanel.repaint();
+        myPanel.setSymbols("1, 2, 3");
 
+        f.add(myPanel);
+        f.pack();
+        f.setVisible(true);
     }
-    public int something() {
-
-    return 0;
-    };
 }
