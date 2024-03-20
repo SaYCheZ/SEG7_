c080ff23bd8a9c672faa6f904df67a6cc042c9c6
jenah <jenahah85@mail.ru>
2024-03-20 17:19:01 +0300

    Вывод семи сегментов

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,79 +1,131 @@
 import javax.swing.*;
+
 import java.awt.*;
+
 import java.util.ArrayList;
+
 
 public class MyPanel extends JPanel {
 
-    public MyPanel() {
-        setBorder(BorderFactory.createLineBorder(Color.black));
-    }
-
-    public Dimension getPreferredSize() {
-        return new Dimension(1560, 700);
-    }
 
     private int x = 100;
+
     private int y = 100;
+
     private int width = 200;
+
     private int height = 200;
+    private int step = 30;
+
     private int delta = 10;
+
     private ArrayList<Rectangle> outlist = new ArrayList<Rectangle>();
 
-    public void setSymbols(String parm) {
-        segment(1);
-        segment(2);
-        segment(3);
-        segment(4);
-        segment(5);
-        segment(6);
-        segment(7);
+
+    public MyPanel() {
+
+        setBorder(BorderFactory.createLineBorder(Color.black));
+
     }
+
+
+    public Dimension getPreferredSize() {
+
+        return new Dimension(1560, 700);
+
+    }
+
+
+    public void setSymbols(String parm) {
+
+        String[] arr = parm.replace(",", "").split(" ");
+        for (String cifra : arr) {
+            switch (cifra) {
+
+                case "1":
+
+                    segment(2);
+
+                    segment(4);
+
+                    break;
+
+                case "2":
+
+                    segment(1);
+
+                    segment(2);
+
+                    segment(3);
+
+                    segment(6);
+
+                    segment(5);
+
+                    break;
+
+                case "3":
+
+                    segment(1);
+
+                    segment(2);
+
+                    segment(3);
+
+                    segment(4);
+
+                    segment(5);
+
+                    break;
+
+            }
+
+            x = x + step + width;
+
+        }
+    }
+
 
     private void segment(int number) {
         switch (number) {
             case 1:
-                outlist.add(new Rectangle(x, y, width - delta, y));
+                outlist.add(new Rectangle(x+delta, y, x+width - delta, y));
+
                 break;
 
             case 2:
 
-
-                outlist.add(new Rectangle(width, y + delta, width, y+(height / 2) - delta));
+                outlist.add(new Rectangle(x+width, y + delta, x+width, y + (height / 2) - delta));
 
                 break;
 
             case 3:
 
-
-                outlist.add(new Rectangle(x + delta, y + height / 2, width - delta, y+height / 2));
+                outlist.add(new Rectangle(x + delta, y + height / 2, x+width - delta, y + height / 2));
 
                 break;
 
             case 4:
 
-
-                outlist.add(new Rectangle(width, y +( height / 2 + delta), width, y+height - delta));
+                outlist.add(new Rectangle(x+width, y + (height / 2) + delta, x+width, y + height - delta));
 
                 break;
 
             case 5:
 
-
-                outlist.add(new Rectangle(x + delta, y + height, width - delta, y+height));
+                outlist.add(new Rectangle(x + delta, y + height, x+width - delta, y + height));
 
                 break;
 
             case 6:
 
-
-                outlist.add(new Rectangle(x, y + (height / 2) + delta, x, y+height - delta));
+                outlist.add(new Rectangle(x, y + (height / 2) + delta, x, y + height - delta));
 
                 break;
 
             case 7:
 
-
-                outlist.add(new Rectangle(x, y + delta, x, y+(height / 2) - delta));
+                outlist.add(new Rectangle(x, y + delta, x, y + (height / 2) - delta));
 
                 break;
 
@@ -81,13 +133,27 @@ public class MyPanel extends JPanel {
 
     }
 
+
     public void paintComponent(Graphics g) {
+
         super.paintComponent(g);
 
 
         g.setColor(Color.BLACK);
+
+        g.setFont(new Font("Arial", Font.BOLD, 16));
+
+        g.drawString("Drawing shapes using ArrayList", 10, 25);
+
+
+        g.setColor(Color.BLUE);
+
         for (Rectangle rectangle : outlist) {
-            g.drawRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+
+            g.drawLine(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+
         }
+
     }
+
 }
